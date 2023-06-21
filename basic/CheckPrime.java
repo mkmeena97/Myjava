21272252ea233d5b54a08ad050fba41a0052f493
Mahendra Kumar Meena <136839661+mkmeena97@users.noreply.github.com>
2023-06-21 17:10:42 +0530

    Create CheckPrime.java
    check whether number is prime or not

---
+++ basic/CheckPrime.java
@@ -0,0 +1,38 @@
+
+public class CheckPrime{
+  public static boolean isPrime(int n)
+  {
+    if(n==2)
+    {
+      return true;
+      break;
+    }
+    if(n<2)
+    {
+      return false;
+      break;
+    }
+    else
+    {
+      boolean flag=true;
+    for(int i=2;i<n;i++)
+      {
+        if(n%i==0)
+          flag=false;
+      }
+    if(flag==true)
+      return true;
+    else
+      return false;
+    }
+  }
+  public static void main(String [] args)
+  {
+    Scanner sc = new Scanner(System.in);
+      System.out.println("Enter number ");
+    int n=sc.nextInt();
+    System.out.println(isPrime(n));
+                        
+  }
+  
+}
